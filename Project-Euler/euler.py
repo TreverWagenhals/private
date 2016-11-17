7394cafa938f4ec561eed7e464b28bba021c54f4
TreverWagenhals <trever.wagenhals@gmail.com>
2016-11-17 17:54:47 +0300

    Completed additional problems

--- Project-Euler/euler.py
+++ Project-Euler/euler.py
@@ -1,72 +1,58 @@
-import math
+from math import *
 import time
-from euler_funcs import isPrime
-from euler_funcs import primesfrom2to
-from euler_funcs import divide
-from euler_funcs import factors
+from euler_funcs import *
 from decimal import *
 from sets import *
 from sympy import sieve
 import numpy
-import itertools
-from itertools import permutations
+from itertools import *
 from fractions import Fraction
 
 def problem_99():
-    f = open('problem_99.txt', 'r')
+    f = open('problem_99.txt', 'r')                                         # Read in the file that contains base and exponent combination values                                           
     largest_line_value = 0
-
-    for i in range(1000):
-        string_line = f.readline()
-        first_value_found = 0
-        string_checker = 1
+    for i in range(1000):                                                     # Go through all 1000 lines of code
+        string_line = f.readline().split(",")                              # Store line in list, splitting base and exponent
         
-        while (first_value_found == 0):
-            try: 
-                int(string_line[0:string_checker])
-            except:
-                first_value_found = 1 #The value has been found
-                first_value = int(string_line[0:string_checker-1])
-                second_value = int(string_line[string_checker:])
-                exponent = second_value * math.log10(first_value)
-         
-                if exponent > largest_line_value:
-                    largest_line_value = exponent
-                    largest_line = i + 1
-                
-            string_checker = string_checker + 1
-            
+        base = float(string_line[0])                                        # Save first value to base variable, converting to float                            
+        exponent = float(string_line[1])                                    # Save second value to exponent variable, converting to float
+        solution = exponent * math.log10(base)                             # A log solution should have the same relative values, so the greatest number can still 
+                                                                                # be found without having insanely large numbers
+        if solution > largest_line_value:                                  # If solution is greater than previous greatest solution found, update it
+             largest_line_value = solution
+             largest_line = i + 1                                            # Get the line number for new largest solution. Line is +1 since list starts at 0, not 1
+                           
     print largest_line    
     
 def problem_13():
-    f = open('problem_23.txt', 'r')
+    f = open('problem_23.txt', 'r')                         # Open file
     total = 0
-    for i in range(0,100):
-        total += int(f.readline())
-    print str(total)[0:10]
+    for i in range(0,100):                                   # 100 lines of text, go through each line
+        total += int(f.readline())                          # Add line value to total
+    print str(total)[0:10]                                  # Print last 10 digits of total
     
 def problem_48():
     total = 0
-    for i in range(1,1001):
-        total += pow(i, i)
-    print str(total)[-10:]
+    for i in range(1,1001):                             # Loop through all 1000 values
+        total += pow(i, i)                               # Add i^i to total
+    print str(total)[-10:]                              # Print last 10 digits of total
     
 def problem_56():
-    largest = 0
-    for i in range(1,101):
-        for j in range(1,101):
-            sum = 0
-            exponent = pow(i,j)
-            for k in str(exponent):
+    largest = 0                         
+    for i in range(1,101):                          # Base value 1-100
+        for j in range(1,101):                      # Exponent value 1-100
+            sum = 0                                   # Reset sum for current variable digits
+            exponent = pow(i,j)                     # Calculate power value
+            for k in str(exponent):                 # Loop through value and add each digit to sum
                 sum += int(k)
-            if sum > largest:
+            if sum > largest:                       # If the sum of the digits is larger than the previous largest, update
                 largest = sum
              
     print largest
     
 def problem_16():
     total = 0
-    for i in str(2**1000):
+    for i in str(2**1000):                      # Simply loop through all digits in 2^1000 and add them up
         total += int(i)
         
     print total
@@ -74,38 +60,38 @@ def problem_16():
 
 def problem_55():
 	ly_count = 0
-	for i in range(1,10000):
-		it_count = 0
+	for i in range(1,10000):                        # The problem states to use 10000
+		it_count = 0                                 # Keep track of how many iterations are gone through to check if a number is ly
 		temp = i
-		for max_iterations in range(1,51):
-			temp = temp + int(str(temp)[::-1])
-			if temp == int(str(temp)[::-1]):
+		for max_iterations in range(1,51):       # Go through the maximum amount of iterations specified
+			temp = temp + int(str(temp)[::-1])  # Add inverse of current value to itself
+			if temp == int(str(temp)[::-1]):    # Check to see if the inverse of the new value makes a palindrome
 				break
-			else:
+			else:                                   # If it doesn't, add one to the it_counter.
 				it_count = it_count + 1 
 		
-			if it_count == 50:
-				ly_count = ly_count + 1
+			if it_count == 50:                     # If the counter hits 50, we have found a ly number
+				ly_count = ly_count + 1          # Add one to the ly_counter
 				
 	print ly_count
 		
 			
 			
 def problem_87():    
-    square_primes = primesfrom2to(int(50000000**.5))
+    square_primes = primesfrom2to(int(50000000**.5))                  # Create lists for primes of squares, cubes, and quads
     cube_primes = primesfrom2to(int(50000000**(.3333)))
     quad_primes = primesfrom2to(int(50000000**.25))
 
-    x = set([])
+    x = set([])                                                             
     
-    for j in square_primes:
-        pow2 = pow(j,2)
-        for k in cube_primes:
+    for j in square_primes:                                         # Sort through square primes, trying each one as the current pow2
+        pow2 = pow(j,2)                                              
+        for k in cube_primes:                                       # Do the same for cube primes
                 pow3 = pow(k,3)
-                for l in quad_primes:
+                for l in quad_primes:                               # And quad primes
                         pow4 = pow(l,4)
-                        total = pow2 + pow3 + pow4
-                        if total <= 50000000:
+                        total = pow2 + pow3 + pow4                  # Get the total of these values
+                        if total <= 50000000:                       # If the total < 50mil, it is possible and add it to the set 
                             x.add(total)                                                 
     print len(x)
 				
@@ -113,48 +99,48 @@ def problem_87():
 def problem_145():
 	counter = 0
 	def reversible(n):
-		if (n % 10 == 0):
+		if (n % 10 == 0):                    # Quickly rule out any divisble by 10 since term can't end in a 0 for this to work
 			return False
-		sum = i + int(str(i)[::-1])
+		sum = i + int(str(i)[::-1])        # add reversed number to original
 		while sum > 0:
-			if ((sum % 10) % 2) == 0:
-				return False
-			sum /= 10
+			if ((sum % 10) % 2) == 0:      # We want to check each digit, so we divide by 10 to check the next digit. We are checking to see if they are odd,
+				return False               # so we divide by 2 to see if they're odd
+			sum /= 10                       # decrement amount of digits to continue process. If the sum gets to 0 without finding an even digit, it will print true
 		return True
 	
-	for i in xrange (1, 100000000):
-		if reversible(i):
-			counter = counter + 1
+	for i in xrange (1, 100000000,2):       # Couldn't think of any method aside from bruteforce. Was able to iterate by 2 though because any number that is a palindrome
+		if reversible(i):                    # will begin with even and end in odd, or vice-versa, to make an odd number in the end
+			counter = counter + 1           # So, we only need to check for one of the solutions by looking at odds then double it.
 			
-	print counter
+	print counter*2
 			
   
 def problem_30():
 	total = 0
-	for i in range(2, 9**6):
-		temp = 0
+	for i in range(2, 9**6):                # The limit is 9^6 because past 9^6, then the amount of digits in the starting value are by a factor of at least 10 larger
+		temp = 0                              # than the power of all digits added together. So, this is where it becomes impossible for a solution to be found.
 		
-		for length in str(i):
+		for length in str(i):              # Go through the value, and raise each digit to the power of 5 and add up
 			temp += int(length)**5
 		
-		if temp == i:
+		if temp == i:                       # If power value equals original, increase counter
 			total += temp
 			
 	print "TOTAL IS", total
 	
-def problem_18_67(problem):
+def problem_18_67(problem):             # Managed to combine to problems into one since they are the same, just call on different triangles of different sizes
     if problem == 18:
         size_of_triangle = 15
-        f = open('problem_18.txt', 'r')
+        f = open('problem_18.txt', 'r')     # Read in proper text file and triangle size based on problem number specified
     elif problem == 67:
         size_of_triangle = 100
         f = open('problem_67.txt', 'r')
         
     rows = []
     
-    for i in range(0,size_of_triangle):
+    for i in range(0,size_of_triangle):                         # Add each row of the text to the list, splitting the values based on spaces
         rows.append(f.readline().strip().split(" "))
-        map(int, rows[i])
+        map(int, rows[i])                                         # Make everything an int for future use
     rows = [map(int, x) for x in rows]
     
     for row in range(size_of_triangle-1,0,-1):
@@ -466,18 +452,78 @@ def problem_45():
     
 def problem_49():
     prime_list = []
-    for i in range(1000, 10001):
+    for i in range(1000, 10001):    # The problem states a 4 digit value, so only get 4 digit value prime numbers and store in list
         if isPrime(i):
             prime_list.append(i)
             
-    for j in range(0, len(prime_list)):
-        for k in range(j+1, len(prime_list)):
-            difference = prime_list[k] - prime_list[j]
-            if prime_list[k] + difference in prime_list:
-                if sorted(str(prime_list[j])) == sorted(str(prime_list[k])) == sorted(str(prime_list[k] + difference)):
-                    print str(prime_list[j]) + str(prime_list[k]) + str(prime_list[k] + difference)
+    for j in range(0, len(prime_list)):                             # Iterate through each list of primes
+        for k in range(j+1, len(prime_list)):                       # Go through all values greater than current prime value j
+            difference = prime_list[k] - prime_list[j]             # determine difference between these values greater than j and j
+            if prime_list[k] + difference in prime_list:           # check to see if there is a third value that increments by variable difference in the list
+                if sorted(str(prime_list[j])) == sorted(str(prime_list[k])) == sorted(str(prime_list[k] + difference)): # Now that we found an incremental pattern, verify if numbers are identical
+                    print str(prime_list[j]) + str(prime_list[k]) + str(prime_list[k] + difference)     # Concatenate all three values and print it
+                    
+def problem_15():
+    """ PROGRAMMING APPROACH USING CONCEPT OF PASCAL TRIANGLE """
+    def pascal(n, show = True):
+        global r1
+        r1 = [1]
+        r2 = [1, 1]
+        degree = 1
+        while degree <= n:
+            r1 = r2
+            r2 = [1] + [sum(pair) for pair in zip(r2, r2[1:]) ] + [1]
+            degree += 1
+            if show == True:
+                print r1
             
+    def grid_combinations(squares, show):
+        global r1
+        pascal(squares*2, show)
+        print r1[squares]
     
+    grid_combinations(20, show = False)
+    
+    """ ONE LINER USING STATISTICAL APPROACH """
+    print factorial(40) / (factorial(20)**2)
+    
+def problem_28():
+    total = 1                               # The center is the only one that doesn't have 4 corners, so I set the start as the center value
+    current = 1                             # current keeps track of the value of the current corner. Because 1 is the start, so too is the current
+    for i in range(3, 1002, 2):           # The grid is 1001x1001. Because there are 2 corners per row/column, we need to iterate by 2
+        for k in range(1,5):               # There are 4 values for each square created
+            current += (i-1)                # Updates the value of the current corner. As rows are added, the current value increases by (i-1)
+            total += current                # add current value to total value to get all corners
+            
+    print total
+    
+def problem_29():
+    terms = set()
+    for a in range(2,101):                  # Go through all values for a in the formula a^b
+        for b in range(2, 101):             # Go through all values for b in the formaul a^b
+            terms.add(a**b)                 # Add the value to a set, which automatically removes duplicates
+            
+    print len(terms)
+    
+def problem_34():
+    total = 0
+    # Initially, I had just guessed 100,000 as the limit. I found the answer rather quickly because
+    # the limit was actually ~41k due to the solution. Using math to determine the actual limit though,
+    # the answer would be somewhere above 9!. Just to be safe and lazy, I chose 10!. Although, going back after
+    # the actual limit was found to be 9!*7. This is where the having an additional digit for a the starting value
+    # would not increase the number of digits in the factorial sum.
+    for loops in range(1,factorial(10)):                                
+        sum = 0                                 # Reset factorial sum for each number loop                          
+        for k in str(i):                       # Add the factorial of each digit in current number to sum
+            sum += factorial(int(k))       
+            if sum > i:                         # Don't waste time factoring if sum already exceeds initial number
+                break
+            elif sum == i:                      # If sum = initial number, a solution has been found, add to total
+                total += i
+                
+    print total
+        
+
 start_time = time.time()
-problem_49()
+problem_145()
 print (time.time() - start_time), "Seconds"
