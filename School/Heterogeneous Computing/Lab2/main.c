78e35dc1f573df8f219c07339f2777d4bc978aba
TreverWagenhals <trever.wagenhals@gmail.com>
2018-11-18 18:04:30 -0500

    test

--- School/Heterogeneous Computing/Lab2/main.c
+++ School/Heterogeneous Computing/Lab2/main.c
@@ -33,8 +33,8 @@ int main()
     cl_kernel kernel = NULL;
 
     cl_uint num_comp_units;
-    cl_uint global_size;
-    cl_uint local_size;
+    size_t global_size;
+    size_t local_size;
 
     float *result;
     FILE *fp;
@@ -155,12 +155,12 @@ int main()
 
     int iterations = 16;
     int *numIterations = &iterations;
-    cl_uint *numWorkers = &global_size;
+    int *numWorkers = (int *)&global_size;
     /* Create kernel argument */
     ret = clSetKernelArg(kernel, 0, sizeof(int), &numIterations);
     ret |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &result_buffer);
     ret |= clSetKernelArg(kernel, 2, sizeof(float), NULL);
-    ret |= clSetKernelArg(kernel, 3, sizeof(numWorkers), &numWorkers);
+    ret |= clSetKernelArg(kernel, 3, sizeof(int), &numWorkers);
     if(ret < 0) 
     {
        printf("Couldn't set a kernel argument");
